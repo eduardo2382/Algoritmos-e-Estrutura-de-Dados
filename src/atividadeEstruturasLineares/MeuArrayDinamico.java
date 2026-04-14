fa25deb26e974cded5cd2ffc48e84e34db6c6659
eduardo2382 <motadossantosjoseeduardo.m82@gmail.com>
2026-04-14 20:19:27 -0300

    exercicios 1-4

---
+++ src/atividadeEstruturasLineares/MeuArrayDinamico.java
@@ -0,0 +1,99 @@
+package atividadeEstruturasLineares;
+
+// 1.
+public class MeuArrayDinamico {
+    private int[] dados;
+    private int tamanho; // quantos elementos realmente existem
+
+    public MeuArrayDinamico(int capacidadeInicial) {
+        this.dados = new int[capacidadeInicial];
+        this.tamanho = 0;
+    }
+
+    /** Dobra a capacidade do array interno . */
+    private void resize() {
+        // TODO 1a: crie um novo array com o dobro do tamanho
+        int[] newArray = new int[(dados.length * 2)];
+
+        // TODO 1b: copie cada elemento de ’dados ’ para o novo array
+        for (int i = 0; i < dados.length; i++) {
+            newArray[i] = dados[i];
+        }
+
+        // TODO 1c: faca ’dados ’ apontar para o novo array
+        dados = newArray;
+    }
+
+    /** Adiciona ’valor ’ no final do array . */
+    public void add(int valor) {
+        // TODO 2a: se tamanho == dados . length , chame resize ()
+        if (tamanho == dados.length) {
+            resize();
+        }
+
+        // TODO 2b: coloque ’valor ’ na posicao ’tamanho ’
+        dados[tamanho] = valor;
+
+        // TODO 2c: incremente ’tamanho ’
+        tamanho++;
+    }
+
+    /** Retorna o elemento na posicao ’indice ’. */
+    public int get(int indice) {
+        if (indice < 0 || indice >= tamanho)
+            throw new IndexOutOfBoundsException(" Indice invalido : " + indice);
+
+        return dados[indice];
+    }
+
+    public int size() {
+        return tamanho;
+    }
+
+    /** Imprime o array no formato [a, b, c]. */
+    public void imprimir() {
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(dados[i]);
+            if (i < tamanho - 1)
+                sb.append(", ");
+        }
+        sb.append("]");
+        System.out.println(sb.toString());
+    }
+
+    /** Remove o elemento na posicao ’indice ’. */
+    public void remove(int indice) {
+        if (indice < 0 || indice >= tamanho)
+            throw new IndexOutOfBoundsException(" Indice invalido : " + indice);
+
+        // TODO 1: desloque cada elemento de ( indice +1) ate ( tamanho -1)
+        // uma posicao para a esquerda .
+        // Ou seja : dados [i] = dados [i +1] ( para i de indice ate tamanho -2)
+
+        for(int i = indice; i < (tamanho-1); i++){
+            dados[i] = dados[i+1]; 
+        }
+
+        // TODO 2: decremente ’tamanho ’
+        tamanho--;
+    }
+
+    // === Teste ===
+    public static void main(String[] args) {
+        MeuArrayDinamico arr = new MeuArrayDinamico(2);
+        arr.add(10);
+        arr.add(20);
+        arr.imprimir(); // [10 , 20]
+        arr.add(30); // deve fazer resize aqui !
+        arr.add(40);
+        arr.imprimir(); // [10 , 20 , 30 , 40]
+        System.out.println("Tamanho: " + arr.size()); // 4
+
+        arr.remove(1);
+        arr.imprimir();
+        arr.remove(0);
+        arr.imprimir();
+        System.out.println("Tamanho: " + arr.size()); // 4
+    }
+}
---
+++ src/atividadeEstruturasLineares/Node.java
@@ -0,0 +1,19 @@
+package atividadeEstruturasLineares;
+
+public class Node {
+    public Object item;
+    public Node next;
+
+    // TODO : crie um construtor Node ( Object item ) que
+    // inicializa this . item e coloca next = null
+    public Node(Object item){
+        this(item, null);
+    }
+
+    // TODO : crie um segundo construtor Node ( Object item , Node next )
+    // que permite ja encadear na criacao
+    public Node(Object item, Node next){
+        this.item = item;
+        this.next = next;
+    }
+}
