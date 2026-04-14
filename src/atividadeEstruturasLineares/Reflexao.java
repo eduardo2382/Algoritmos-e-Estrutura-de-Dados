77a1f32f8e5bc7d9e903f126c2b44c0e09285120
eduardo2382 <motadossantosjoseeduardo.m82@gmail.com>
2026-04-14 20:47:38 -0300

    ultimos exercicios

---
+++ src/atividadeEstruturasLineares/Reflexao.java
@@ -0,0 +1,9 @@
+package atividadeEstruturasLineares;
+
+/* 
+    a) Em uma lista encadeada apenas é preciso mudar o endereco do nó antes da posicao que precisamos inserir e referenciar no novo no o no da posicao+1 levando a um custo de O(1), ja no array precisamos percorrer o restante do array depois da posicao para podermos "empurrar" aqueles elementos para frente
+
+    b)Array, apenas precisamos pegar o elemento pelo ultimo indice, na lista encadeada precisariamos percorrer ele toda atraves dos nexts
+
+    c)Sim, seria menos eficiente ja que a cada remocao precisaria percorrer todo o array para manipular as posicoes dos items
+*/
